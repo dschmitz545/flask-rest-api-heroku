05f6c06cf7b8c88e12bd10bd99d0cffaefa488ef
Diego Schmitz <diego.sctar@gmail.com>
2020-01-19 23:14:20 -0300

    corrigido erro do heroku

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
 
-from db import db
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.user import UserRegister
@@ -14,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'diego'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # aqui já vai criar a rota /auth também
 
@@ -33,5 +28,6 @@ api.add_resource(UserRegister, '/register')
 # se esse arquivo for usado para ser importado em outro arquivo
 # essa verificação baixo não será executado, pois esse arquivo não será o arquivo main do projeto
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=8000, debug=True)
